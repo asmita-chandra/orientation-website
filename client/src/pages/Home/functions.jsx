c30d1efc25746a9188c510b4aa592594f7a3187b
Gaurika Mahajan <gaurikam2003@gmail.com>
2024-06-25 13:54:36 -0400

    Changed home page photos and fixed cursor issue

--- client/src/pages/Home/functions.jsx
+++ client/src/pages/Home/functions.jsx
@@ -4,23 +4,23 @@ const { axios } = useAxios();
 import slideshow1 from '../../assets/homeSlideshow/NatalieChan_Gamma (7)-X3.webp';
 import slideshow2 from '../../assets/homeSlideshow/DSC_0309_reduced.webp';
 import slideshow3 from '../../assets/homeSlideshow/DSC_0411_reduced.webp';
-import slideshow4 from '../../assets/homeSlideshow/EgeFeyzioglu_Phi (264)-X3.webp';
+import slideshow4 from '../../assets/homeSlideshow/cjancheta_alpha_9375-L.webp';
 import slideshow5 from '../../assets/homeSlideshow/Erin_Sigma_CheerOff (30)-X3.webp';
 import slideshow6 from '../../assets/homeSlideshow/IMG_9541_reduced.webp';
 import slideshow7 from '../../assets/homeSlideshow/MithunVanniasinghe_Omega-0422-X3.webp';
 import slideshow8 from '../../assets/homeSlideshow/NINU2-350-X3.webp';
-import slideshow9 from '../../assets/homeSlideshow/ZarehMarkarian_Chi_04649-X3.webp';
+import slideshow9 from '../../assets/homeSlideshow/cjancheta_sigma_9294-L.webp';
 import slideshow10 from '../../assets/homeSlideshow/VedantGupta_Pi-7292-X3.webp';
 
 import slideshowTiny1 from '../../assets/homeSlideshow/tiny/NatalieChan_Gamma (7)-4K.jpg';
 import slideshowTiny2 from '../../assets/homeSlideshow/tiny/DSC_0309.webp';
 import slideshowTiny3 from '../../assets/homeSlideshow/tiny/DSC_0411.webp';
-import slideshowTiny4 from '../../assets/homeSlideshow/tiny/EgeFeyzioglu_Phi (264)-4K.jpg';
+import slideshowTiny4 from '../../assets/homeSlideshow/tiny/cjancheta_alpha_9375-Ti.webp';
 import slideshowTiny5 from '../../assets/homeSlideshow/tiny/Erin_Sigma_CheerOff (30)-4K.jpg';
 import slideshowTiny6 from '../../assets/homeSlideshow/tiny/IMG_9541.webp';
 import slideshowTiny7 from '../../assets/homeSlideshow/tiny/MithunVanniasinghe_Omega-0422-4K.jpg';
 import slideshowTiny8 from '../../assets/homeSlideshow/tiny/NINU2-350-4K.jpg';
-import slideshowTiny9 from '../../assets/homeSlideshow/tiny/ZarehMarkarian_Chi_04649-XL.jpg';
+import slideshowTiny9 from '../../assets/homeSlideshow/tiny/cjancheta_sigma_9293-Ti.webp';
 import slideshowTiny10 from '../../assets/homeSlideshow/tiny/VedantGupta_Pi-7292-4K.jpg';
 
 export async function getTimelineEvents() {
