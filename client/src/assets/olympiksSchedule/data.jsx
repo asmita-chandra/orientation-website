37f960b2d1c32ed31521218369a2edf3c589607a
Ashley Leal <anleal2004@gmail.com>
2024-08-19 05:42:00 -0400

    create data file for olympiks schedule, add mse sf pit -bathroom-side tables

---
+++ client/src/assets/olympiksSchedule/data.jsx
@@ -0,0 +1,52 @@
+export const dataMSE = {
+  'SF Pit - Bathroom-side tables': [
+    {
+      'Activity Name': 'Chess: Assorted Games',
+      'Start Time': '9:00:00 a1/p1',
+      'End Time': '11:00:00 a1/p1',
+      'Activity Description':
+        'Come play chess against Frosh from other disciplines!! Win points for your discipline to win the Skule Kup!',
+      'Participant Limit': 'No sign ups',
+    },
+
+    {
+      'Activity Name': 'Chess: Beginner Hour!',
+      'Start Time': '11:00:00 a1/p1',
+      'End Time': '12:00:00 a1/p1',
+      'Activity Description':
+        'Haven’t played chess before or consider yourself a beginner? Come play others at the beginner skill level for a fun and light-hearted game to earn Skule Kup points!',
+      'Participant Limit': 'Unlimited',
+    },
+    {
+      'Activity Name': 'Lunch - Break',
+      'Start Time': '12:00:00 a1/p1',
+      'End Time': '13:00:00 a1/p1',
+      'Activity Description': 'Take a break and grab some lunch!',
+      'Participant Limit': 'No sign ups',
+    },
+    {
+      'Activity Name': 'Chess Tournament',
+      'Start Time': '13:00:00 a1/p1',
+      'End Time': '15:00:00 a1/p1',
+      'Activity Description':
+        'Represent MSE in a Chess Tournament run by HH Chess Club. Limited sign-ups per discipline.',
+      'Participant Limit': 2,
+    },
+    {
+      'Activity Name': 'Chess: Assorted Games',
+      'Start Time': '15:00:00 a1/p1',
+      'End Time': '15:30:00 a1/p1',
+      'Activity Description':
+        'Come play chess against Frosh from other disciplines!! Win points for your discipline to win the Skule Kup!',
+      'Participant Limit': 'No sign ups',
+    },
+    {
+      'Activity Name': 'Chess: Advanced Hour!',
+      'Start Time': '15:30:00 a1/p1',
+      'End Time': '17:00:00 a1/p1',
+      'Activity Description':
+        'Are you good at chess? Come meet and play other Frosh who enjoy chess and play at an intermediate-advanced level. Earn points for your discipline!!',
+      'Participant Limit': 'Unlimited',
+    },
+  ],
+};
