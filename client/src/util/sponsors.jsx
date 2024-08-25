378a66fa2eee475efc7d7fb15c6bd60fd86737fc
Gaurika Mahajan <gaurikam2003@gmail.com>
2024-08-25 11:27:20 -0400

    updated sponsors and schedule to include new timings

--- client/src/util/sponsors.jsx
+++ client/src/util/sponsors.jsx
@@ -5,10 +5,7 @@ import Nani from '../assets/sponsors/Nanis.png';
 import Longos from '../assets/sponsors/Longos.png';
 import Troost_iLead from '../assets/sponsors/Troost_iLead.jpg';
 import Panago from '../assets/sponsors/Panago.png';
-// import Neo from '../assets/sponsors/Neo.jpg';
-// import Coco from '../assets/sponsors/Coco.jpg';
-// import UTSU from '../assets/sponsors/UTSU.jpg';
-// import Mogu from '../assets/sponsors/MoguMogu.png';
+
 export const sponsors = [
   {
     website: 'https://alumni.engineering.utoronto.ca/engineering-alumni-network/',
@@ -16,6 +13,20 @@ export const sponsors = [
     scale: 0.82, // can be used to display each image at a different size
     rank: 'diamond', // rank of sponsor, can be used to display border
     label: 'Diamond sponsor: Alumni Office', // the label when user hovers on image in 'View All' state
+  },
+  {
+    website: 'https://www.panago.com/',
+    image: Panago,
+    scale: 0.82,
+    rank: 'diamond',
+    label: 'Diamond sponsor: Panago',
+  },
+  {
+    website: 'https://ospe.on.ca/',
+    image: OSPE,
+    scale: 0.82,
+    rank: 'silver',
+    label: 'Silver sponsor: OSPE',
   },
   {
     website: 'https://www.peo.on.ca/',
@@ -39,51 +50,10 @@ export const sponsors = [
     label: 'Bronze sponsor: Longos',
   },
   {
-    website: 'https://ospe.on.ca/',
-    image: OSPE,
-    scale: 0.82,
-    rank: 'bronze',
-    label: 'Bronze sponsor: OSPE',
-  },
-  {
     website: 'https://ilead.engineering.utoronto.ca/',
     image: Troost_iLead,
     scale: 0.82,
     rank: 'bronze',
     label: 'Bronze sponsor: Troost iLead',
   },
-  {
-    website: 'https://www.panago.com/',
-    image: Panago,
-    scale: 0.82,
-    rank: 'bronze',
-    label: 'Bronze sponsor: Panago',
-  },
-  // {
-  //   image: Mogu,
-  //   scale: 0.82,
-  //   rank: 'bronze',
-  //   label: 'Bronze sponsor: Mogu',
-  // },
-  // {
-  //   website: 'https://www.neomaterials.com/',
-  //   image: Neo,
-  //   scale: 0.7,
-  //   rank: 'gold',
-  //   label: 'Gold sponsor: Neo Performance',
-  // },
-  // {
-  //   website: 'https://www.cocofreshtea.ca/',
-  //   image: Coco,
-  //   scale: 0.7,
-  //   rank: 'gold',
-  //   label: 'Silver sponsor: Coco',
-  // },
-  // {
-  //   website: 'https://www.utsu.ca/',
-  //   image: UTSU,
-  //   scale: 0.7,
-  //   rank: 'bronze',
-  //   label: 'Bronze sponsor: UTSU',
-  // },
 ];
